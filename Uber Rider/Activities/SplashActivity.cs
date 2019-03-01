04af3af5004b1acc2eec10506e356840467cf2fd
Tomas Garcia <tomasotano25@gmail.com>
2019-02-28 22:11:55 -0400

    add splash screen

---
+++ Uber Rider/Activities/SplashActivity.cs
@@ -0,0 +1,23 @@
+﻿namespace Uber_Rider.Activities
+{
+    using Android.App;
+    using Android.OS;
+    using Android.Support.V7.App;
+
+    [Activity(Label = "@string/app_name", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Thread.Sleep(2000);
+            // Create your application here
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
---
+++ Uber Rider/MainActivity.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Firebase;
+using Firebase.Database;
+
+namespace Uber_Rider
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
+    public class MainActivity : AppCompatActivity
+    {
+        FirebaseDatabase database;
+        Button btntestConnection;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btntestConnection = (Button)FindViewById(Resource.Id.mybutton);
+            btntestConnection.Click += BtntestConnection_Click;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void BtntestConnection_Click(object sender, EventArgs e)
+        {
+            this.InitializeDatabase();
+        }
+
+        void InitializeDatabase()
+        {
+            var app = FirebaseApp.InitializeApp(this);
+            if (app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+                    .SetApplicationId("uber-clone-a1c9b")
+                    .SetApiKey("AIzaSyA_iDmRc4MUWwvUuPxlus7NyywRSzah0IA")
+                    .SetDatabaseUrl("https://uber-clone-a1c9b.firebaseio.com")
+                    .SetStorageBucket("uber-clone-a1c9b.appspot.com")
+                    .Build();
+
+                app = FirebaseApp.InitializeApp(this, options);
+                database = FirebaseDatabase.GetInstance(app);
+            }
+            else
+            {
+                database = FirebaseDatabase.GetInstance(app);
+            }
+
+            DatabaseReference dbref = database.GetReference("UserSupport");
+            dbref.SetValue("Ticket1");
+
+            Toast.MakeText(this, "Completed", ToastLength.Short).Show();
+        }
+    }
+}
